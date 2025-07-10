1a2c330f984daf2266b1d45057ccdc7c130abcc8
Jobro Myburgh <myburghjobro@gmail.com>
2025-07-10 09:18:51 +0000

    Make the user see the log in and sign up screen and i want it to look li

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,27 +1,5 @@
-import { SignUpForm } from "@/components/auth/signup-form";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function SignUpPage() {
-  return (
-    <div className="container flex h-[calc(100vh-3.5rem)] items-center justify-center py-12">
-      <Card className="mx-auto max-w-sm w-full">
-        <CardHeader>
-          <CardTitle className="text-2xl font-headline">Sign Up</CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <SignUpForm />
-          <div className="mt-4 text-center text-sm">
-            Already have an account?{" "}
-            <Link href="/login" className="underline text-primary">
-              Login
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  redirect("/");
 }
