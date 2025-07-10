53af0a52e3114d955aaad25bad579d2d6da5d911
Jobro Myburgh <myburghjobro@gmail.com>
2025-07-10 09:35:35 +0000

    What shoud i do to help solve this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAaJRgNkNUTeJhTVAu0IcAJNfsqmL987-U",
+  authDomain: "apptest-b0690.firebaseapp.com",
+  projectId: "apptest-b0690",
+  storageBucket: "apptest-b0690.appspot.com",
+  messagingSenderId: "439946723338",
+  appId: "1:439946723338:web:74f68cd13b6fc9fff29ff3"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
